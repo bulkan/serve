578ddba21ef1f78d32eaf3b9d78c17d39acf304d
Bulkan Evcimen <bulkan@gmail.com>
2013-05-06 20:06:18 +1000

    handle command line arguments

--- serve.go
+++ serve.go
@@ -3,17 +3,27 @@ package main
 import (
     "net/http"
     "log"
+    "fmt"
     "flag"
 )
 
 func main() {
 
     dirPtr := flag.String("dir", ".", "directory to serve")
+    portPtr := flag.String("port", "4242", "port number")
 
     flag.Parse()
 
+    fmt.Printf("Serving %s on port %s\n", *dirPtr, *portPtr)
+
+    if (*portPtr)[0] != ':' {
+        *portPtr = ":" + *portPtr
+    }
+
+    dir := http.Dir(*dirPtr)
+
     err := http.ListenAndServe(":4242",
-        http.FileServer(http.Dir(*dirPtr)))
+        http.FileServer(dir))
 
     if err != nil {
         log.Printf("Error running web server for static assets: %v", err)
