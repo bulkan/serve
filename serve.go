41d5b3528d129f0cc864dc6b82f4cd2bb63cfc9b
Bulkan Evcimen <bulkan@gmail.com>
2013-05-06 20:25:30 +1000

    updated comments

--- serve.go
+++ serve.go
@@ -9,21 +9,21 @@ import (
 
 func main() {
 
+    // handle command line options
     dirPtr := flag.String("dir", ".", "directory to serve")
     portPtr := flag.String("port", "4242", "port number")
-
     flag.Parse()
 
     fmt.Printf("Serving %s on port %s\n", *dirPtr, *portPtr)
 
+    // port number needs colon prepended to it
     if (*portPtr)[0] != ':' {
         *portPtr = ":" + *portPtr
     }
 
     dir := http.Dir(*dirPtr)
 
-    err := http.ListenAndServe(":4242",
-        http.FileServer(dir))
+    err := http.ListenAndServe(*portPtr, http.FileServer(dir))
 
     if err != nil {
         log.Printf("Error running web server for static assets: %v", err)
